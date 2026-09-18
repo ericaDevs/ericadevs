65e62f9ce79e1d45a1a4a14fefbe7e396db2130e
ericaDevs <ericahtech@gmail.com>
2026-09-18 13:25:54 +0300

    Configured Cloudinary for static files storage in production

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'Projects',
     'Contacts',
     'Dashboard',
@@ -152,6 +154,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME" : os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY" : os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET" : os.getenv("CLOUDINARY_API_SECRET")
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
